ff178adae3bb008e30965f14ca246de369bcb7ce
Sonja Rouwhorst <s.e.rouwhorst@hva.nl>
2021-02-12 16:01:56 +0100

    add route movielist

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const exphbs  = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const movies = [
+  {title: "the hulk", genre: "Action", jaartal: 1980},
+  {title: "The shawshank redemption", genre: "Drama", jaartal: 1990},
+  {title: "Spidermn", genre: "Comedy", jaartal: 2020}
+]
+
 app.use(express.static('public'))
 
 app.engine('handlebars', exphbs());
@@ -11,9 +17,11 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
   res.render('home',{})
 });
+
 app.get('/movies', (req, res) => {
-    res.send('A list of movies')
+    res.render('listOfMovies', {title: "Lijst van films", movies})
 })
+
 app.get('/movies/:movieId', (req, res) => {
     res.send(`<h1>Detailpage of movie ${req.params.movieId} </h1>`)
 });
