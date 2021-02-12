da6d9bd6e1742ef5dd36147f30ed573340d8ea22
Sonja Rouwhorst <s.e.rouwhorst@hva.nl>
2021-02-12 15:52:58 +0100

    add handlebars template engine

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'))
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home',{})
 });
 app.get('/movies', (req, res) => {
     res.send('A list of movies')
